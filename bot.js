5728cdbc556f65a3aaf0e688f52ff5392422cbd7
ming <kowmingyuan04@gmail.com>
2023-03-20 19:04:33 +0800

    bot commands test

--- bot.js
+++ bot.js
@@ -1,28 +1,53 @@
-const client = new Discord.Client();
-import Discord from "discord.js";
-import Mastodon from "mastodon-api";//import masotodn api client
-import { createInterface } from "readline"; //importing CLI input
-import ENV from "dotenv";//import env var
-import chalk from "chalk";
 
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const Mastodon = require('mastodon-api');//import masotodn api client
+const ENV = require('dotenv');//import env var
+const prefix = '!';
 ENV.config(); //config env var
 
-client.login('MTA4NjIzNzY2MjU2ODAwOTgwOA.GIP7Rd.3vSW2foReATjhl-8XmWfdo6kQ26M3D2kAMrEvo');
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.on("ready", c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
-const M = new Mastodon({
+// Log in to Discord with your client's token
+client.login(process.env.bot_token);
+
+ const M = new Mastodon({
     access_token: process.env.ACCESS_TOKEN,
     //client_key: process.env.CLIENT_KEY,
     //client_secret:process.env.CLIENT_SECRET,
     api_url: "https://tinkertofu.com/api/v1/", 
 });
 
-client.on('message', message => {
-    if (message.content === '!latesttoots') {
-      M.get('accounts/verify_credentials').then(res => {
-        M.get(`accounts/${res.data.id}/statuses`).then(res => {
-          const latestToot = res.data[0];
-          message.channel.send(latestToot.content);
-        });
-      });
+client.on('messageCreate', (message)=>{
+  console.log('received message');
+  if (message.content.startsWith(prefix)){
+    if (message.author.bot) return;
+    if(message.content === '!hello'){
+      message.channel.send('Hello!');
+      console.log('sent message');
     }
-  });
+  }
+  
+  /*
+  if (message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  //Message Array
+  const messageArray = message.content.split("");
+  const argument = messageArray.slice(1);
+  const cmd = messageArray[0];
+
+  //COMMANDS
+  if (command === 'hello'){
+    message.channel.send('Hello!');
+    console.log('sent message');
+  }*/
+
+})
